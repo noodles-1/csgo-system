9d16313b49da311d5cb575cec00148b4370b3361
noodelz <adrianeroa081202@gmail.com>
2024-03-06 20:42:04 +0800

    unit test for license plate detection

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,10 +3,14 @@ import pytesseract
 from ultralytics import YOLO
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-model = YOLO('trained_models/trained_yolov8n.pt')
+vehicle_detection_model = YOLO('trained_models/vehicle_detection/trained_yolov8n.pt')
+lp_detection_model = YOLO('trained_models/lp_detection/trained_yolov8n.pt')
 
 def detect_vehicle(frame):
-    return model.predict(source=frame)
+    return vehicle_detection_model.predict(source=frame)
+
+def detect_license_plate(frame):
+    return lp_detection_model.predict(source=frame)
 
 def validate_license_number(frame):
     return pytesseract.image_to_string(image=frame, lang='eng', config='--psm 10 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
--- tests/unit/main.py
+++ tests/unit/main.py
@@ -13,7 +13,12 @@ from controllers import controller
 class TestControllerMethods(unittest.TestCase):
     # checks if an image has vehicles
     def test_has_vehicle(self):
-        results = controller.detect_vehicle(frame=cv2.imread('test_images/2.jpg'))
+        results = controller.detect_vehicle(frame=cv2.imread('test_images/1.jpg'))
+        self.assertTrue(results[0].boxes)
+    
+    # checks if an image has a license plate
+    def test_has_license_plate(self):
+        results = controller.detect_license_plate(frame=cv2.imread('test_images/2.jpg'))
         self.assertTrue(results[0].boxes)
 
     # checks if an input license plate image's number matches with predicted number
@@ -22,6 +27,6 @@ class TestControllerMethods(unittest.TestCase):
         img = cv2.resize(img, (0,0), fx=3, fy=3)
         img = cv2.GaussianBlur(img, (5,5), 0)
         predicted = controller.validate_license_number(frame=img)
-        self.assertEqual(predicted.strip(), 'ASD1234')
+        self.assertEqual(predicted.strip(), 'FAB3478')
 
 unittest.main()
