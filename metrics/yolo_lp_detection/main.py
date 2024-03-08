85f31107b28d86b3f632cf413ad7c6ea362cb418
noodelz <adrianeroa081202@gmail.com>
2024-03-08 11:56:14 +0800

    added requirements

--- metrics/yolo_lp_detection/main.py
+++ metrics/yolo_lp_detection/main.py
@@ -9,5 +9,9 @@ sys.path.append(root)
 
 from controllers import controller
 
-results = controller.detect_license_plate(frame=cv2.imread('test_images/13.jpg'))
-print(results)
+img = cv2.imread('test_images/1.jpg')
+vehicle_results = controller.detect_vehicle(frame=img)
+x1, y1, x2, y2 = vehicle_results[0].boxes[4].xyxy[0]
+cropped_vehicle = img[int(y1.item()):int(y2.item()), int(x1.item()):int(x2.item())]
+
+controller.detect_license_plate(frame=cropped_vehicle)
