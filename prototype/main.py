29d0f2e59f5e5732af98b64edf6b2982c6943065
noodelz <adrianeroa081202@gmail.com>
2024-03-08 16:17:13 +0800

    some changes

--- prototype/main.py
+++ prototype/main.py
@@ -9,12 +9,10 @@ from control import getCar, readLP, write_csv
 
 # Load model
 model = YOLO('models/vanilla/yolov8x.pt')
-#model = YOLO('models/trained/trained_yolov8s.pt')
 lp_detector_model = YOLO('models/vanilla/yolov8x.pt')
-lp_detector_model = YOLO('./models/lp/trained_yolov8n_1.pt')
 
 # Load video source
-cap = cv2.VideoCapture('testing_video/sample_lp_videoplayback.mp4')
+cap = cv2.VideoCapture('sample.mp4')
 
 # Declarations
 vehicles = [2, 3, 5, 7]
@@ -28,6 +26,37 @@ classNames = {
 
 results = {}
 motTracker = Sort()
+
+# Function to draw bounding box with class name and confidence
+# Modify the bounding_box function to expect only bounding box coordinates
+def bounding_box(frame, box, classNames):
+    x1, y1, x2, y2, score, class_id = box
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (200, 50, 50), 2)
+
+    confidence = math.ceil((score * 100)) / 100
+
+    org = [x1, y1]
+    font = cv2.FONT_HERSHEY_PLAIN
+    fontScale = 2
+    color = (255, 255, 255)
+    thickness = 2
+
+    cv2.putText(frame, classNames[class_id] + ' ' + str(confidence), tuple(org), font, fontScale, color, thickness)
+
+    # Draw bounding boxes
+    '''
+    for detection in detections_:
+        bounding_box(frame, detection, classNames)  # Pass classNames here
+
+
+    # Display frame
+
+    cv2.imshow('Frame', frame)
+    cv2.waitKey(1)
+    '''
+
 
 # Read frames
 frameNumber = -1
@@ -55,24 +84,24 @@ while ret:
             #assign LP
             xcar1, ycar1, xcar2, ycar2, carID = getCar(licensePlate, trackIDs)
 
-            if carID != -1:
-                #crop LP
-                licensePlateCrop = frame[int(y1): int(y2), int(x1): int(x2), :]
+            #if carID != -1:
+            #crop LP
+            licensePlateCrop = frame[int(y1): int(y2), int(x1): int(x2), :]
 
-                #process LP
-                licensePlateCropGray = cv2.cvtColor(licensePlateCrop, cv2.COLOR_BGR2GRAY)
-                _, licensePlateCropThreshold = cv2.threshold(licensePlateCrop, 64, 255, cv2.THRESH_BINARY_INV)
+            #process LP
+            licensePlateCropGray = cv2.cvtColor(licensePlateCrop, cv2.COLOR_BGR2GRAY)
+            _, licensePlateCropThreshold = cv2.threshold(licensePlateCrop, 64, 255, cv2.THRESH_BINARY_INV)
 
-                #read LP
-                licensePlateText, licensePlateTextConfScore = readLP(licensePlateCropThreshold)
+            #read LP
+            licensePlateText, licensePlateTextConfScore = readLP(licensePlateCropThreshold)
 
-                if licensePlateText is not None:
-                    results[frameNumber][carID] = {'car': {'carBox': [xcar1, ycar1, xcar2, ycar2]},
-                                                  'license_Plate': {'lpBox': [x1, y1, x2, y2],
-                                                                    'text': licensePlateText,
-                                                                    'boxScore': score,
-                                                                    'textScore': licensePlateTextConfScore}}
+
+            if licensePlateText is not None:
+                results[frameNumber][carID] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                              'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                'text': licensePlateText,
+                                                                'bbox_score': score,
+                                                                'text_score': licensePlateTextConfScore}}
 
 #results
 write_csv(results, './test.csv')
-
