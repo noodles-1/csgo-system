fb7fce27ed05f17ae6d1d69b77c18409b80ea693
Alex03002 <201820030@fit.edu.ph>
2024-04-27 04:45:44 +0800

    additional changes to otp generator code

--- controllers/otpVerify.py
+++ controllers/otpVerify.py
@@ -6,17 +6,15 @@ def otpGenerator():
     onetimePin = format(random.randint(0,999999), "06d")
     return onetimePin
 
-#function for sending the otp to the user
+#function for sending the otp to the user and setting up the email server and sender address (this is just a temporary email with an app password i created at the date i wrote this code)
 def sendOTP(OTP, inputEmail):
     emailSubject = ("Your OTP - " + OTP)
     emailMessage = ("Hello! Your OTP is " + OTP + ". Use it to verify your account for logging in.")
     emailSent = f"subject:{emailSubject}\n\n{emailMessage}"
+    smtpServer = smtplib.SMTP('smtp.gmail.com',587)
+    smtpServer.starttls()
+    smtpServer.login("csgolpr001@gmail.com","dbfi jcfy febq rhba")
     smtpServer.sendmail("csgolpr001@gmail.com",inputEmail,emailSent)
-
-#set up the email server and the sender email (this is just a temporary email with an app password i created at the date i wrote this code)
-smtpServer = smtplib.SMTP('smtp.gmail.com',587)
-smtpServer.starttls()
-smtpServer.login("csgolpr001@gmail.com","dbfi jcfy febq rhba")
 
 #setup for user's email address
 userEmail = input("Enter your E-Mail address: ")
@@ -27,9 +25,10 @@ sendOTP(mailOTP, userEmail)
 print("Your OTP has been sent to your E-Mail!")
 
 #verifying the OTP that the user will input
-verifyOTP = input("Please enter your OTP: ")
-while verifyOTP != mailOTP:
-    verifyOTP = input("Incorrect OTP. Please enter the correct OTP: ")
+while True:
+    verifyOTP = input("Enter your OTP: ")
     if verifyOTP == mailOTP:
         print("Account verified!")
         break
+    else:
+        print("Invalid OTP. Please try again.")
