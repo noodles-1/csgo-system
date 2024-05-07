de8db2e32b13b54c634013f803ef2da15ca3a477
noodelz <adrianeroa081202@gmail.com>
2024-05-07 16:53:07 +0800

    added unit testing documentation (1.0.9)

--- tests/unit/dbTest.py
+++ tests/unit/dbTest.py
@@ -15,6 +15,17 @@ class TestDBControllerMethods(unittest.TestCase):
     @patch('controllers.dbController.DBController.emailExists')
     @patch('controllers.dbController.DBController.usernameExists')
     def test_register_user(self, mock_username_exists, mock_email_exists, mock_session):
+        '''
+        Tests the registerUser method from DBController. Assumes that the username and email
+        provided does not exist yet and is available for registration. This test case tests the
+        functionality of the registerUser method in identifying missing credentials and invalid
+        email or username format while mocking database transactions.
+
+        asserts:
+        - True => if the user has been successfully registered
+        - False => if the registration has failed caused by either missing credentials or
+        invalid email or username format
+        '''
         mock_username_exists.return_value = False
         mock_email_exists.return_value = False
         mock_session.add = MagicMock()
@@ -27,6 +38,16 @@ class TestDBControllerMethods(unittest.TestCase):
     @patch('controllers.dbController.DBController.emailExists')
     @patch('controllers.dbController.DBController.usernameExists')
     def test_login_user(self, mock_username_exists, mock_email_exists, mock_get_user):
+        '''
+        Tests the loginUser method from DBController. Assumes that the username and email
+        provided exists and can be logged in. This test case tests the functionality of the
+        loginUser method in identifying missing credentials and invalid email or username
+        format while mocking database transactions.
+
+        asserts:
+        - True => if the passwords match, then the user has successfully logged in
+        - False => if the passwords do not match, or the email or username format is invalid
+        '''
         mock_username_exists.return_value = True
         mock_email_exists.return_value = True
         mock_get_user.return_value = MagicMock(password='321')
