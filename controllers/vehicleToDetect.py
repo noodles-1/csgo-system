78809091e891de00175260dfb0b6edbe05f23c69
rouxri <202110938@fit.edu.ph>
2024-05-13 23:49:44 +0800

    1.0.3

--- controllers/vehicleToDetect.py
+++ controllers/vehicleToDetect.py
@@ -1,34 +1,18 @@
-def main():
-    vehicles = ["car", "motorcycle", "jeepney", "bus", "tricycle", "van", "truck", "taxi", "modern_jeepney"]
+classes = ['car', 'motorcycle', 'jeepney', 'bus', 'tricycle', 'van', 'truck', 'taxi', 'modern_jeepney']
+classes_enabled = [False] * 9
 
-    while True:
-        print("Choose a vehicle:")
-        print("0: Car")
-        print("1: Motorcycle")
-        print("2: Jeepney")
-        print("3: Bus")
-        print("4: Tricycle")
-        print("5: Van")
-        print("6: Truck")
-        print("7: Taxi")
-        print("8: Modern Jeepney")
-        print("9: Exit")
+try:
+    i = classes.index('car')
+    classes_enabled[i] = not classes_enabled[i]
 
-        choice = input("Enter the number of the vehicle: ")
+    i = classes.index('taxi')
+    classes_enabled[i] = not classes_enabled[i]
 
-        try:
-            choice = int(choice)
-            if 0 <= choice <= 8:
-                print("You chose:", vehicles[choice])
-            elif choice == 9:
-                print("Exiting the program. Goodbye!")
-                break
-            else:
-                print("Invalid choice. Please enter a number between 0 and 8.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-        except IndexError:
-            print("Invalid choice. Please enter a number between 0 and 8.")
+    i = classes.index('jeepney')
+    classes_enabled[i] = not classes_enabled[i]
 
-if __name__ == "__main__":
-    main()
+    filtered_classes = [i for i in range(len(classes)) if classes_enabled[i]]
+    print(filtered_classes)
+
+except ValueError as e:
+    print(f"Error: {e}. Vehicle type not found.")
