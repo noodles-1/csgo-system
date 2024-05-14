39a34d0767e90c27a56e91fc4e1f55b08c5e371c
noodelz <adrianeroa081202@gmail.com>
2024-05-14 19:09:56 +0800

    added py files

---
+++ README.md
@@ -0,0 +1,12 @@
+# This is the repository of the system for the Thesis of the Group Computer Science Global Offensive
+## This Branch is currently in Version 1.2.0
+
+Members of the thesis group are:
+  - **Pagtalunan, John Gabriel T.**
+  - **Roa, Adriane Gil**
+  - **Rayray, Kassandra Beatriz**
+  - **Mendoza, Mendoza**
+
+Students major in **Software Engineering** in **Far Eastern University Institute of Technology**
+
+Thesis Title: ***Automated Highway Congestion Pricing Implemented Using YOLO and Image Processing Algorithms For Reduced Congestion In NCR.***
---
+++ controllers/userCredentials.py
@@ -0,0 +1,27 @@
+import random
+import smtplib
+
+#function for generating a random 6 digit one-time pin
+def otpGenerator():
+    oneTimePin = format(random.randint(0,999999), "06d")
+    return oneTimePin
+
+#function for sending the otp to the user and setting up the email server and sender address (this is just a temporary email with an app password i created at the date i wrote this code)
+def sendOTP(OTP, inputEmail):
+    emailSubject = ("Your OTP - " + OTP)
+    emailMessage = ("Hello! Your OTP is " + OTP + ". Use it to verify your account for logging in.")
+    emailSent = f"subject:{emailSubject}\n\n{emailMessage}"
+    smtpServer = smtplib.SMTP('smtp.gmail.com',587)
+    smtpServer.starttls()
+    smtpServer.login("csgolpr001@gmail.com","dbfi jcfy febq rhba")
+    smtpServer.sendmail("csgolpr001@gmail.com",inputEmail,emailSent)
+    print("Your OTP has been sent to your E-Mail!")
+
+#function for verifying the OTP that the user will input
+def verifyOTP(inputOTP, receivedOTP):
+    while True:
+        if inputOTP == receivedOTP:
+            return "Account verified!"
+        else:
+            inputOTP = input("Invalid OTP. Please input the correct OTP: ")
+            continue
